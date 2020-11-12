e2be57742a565d7349acd53265ea5f174aa8e003
tomp <you@example.com>
2020-11-12 06:45:33 -0300

    primer commit

---
+++ cliente/src/components/auth/Login.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  // State para el usuario y sus datos
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+})
+
+  // extraer de usuario. De lo contrario dicen que no se definieron
+  const { email, password } = usuario;
+
+const onChange = (e) => {
+  guardarUsuario({
+    ...usuario,
+    [e.target.name] : e.target.value
+  })
+}
+
+const onSubmit = (e) => {
+  e.preventDefault();
+}
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+
+        <form
+          onSubmit={onSubmit}
+        >
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}  
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+        <Link to="/nueva-cuenta" className="enlace-cuenta">
+          ¿No tienes cuenta?
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ cliente/src/components/proyectos/Proyectos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Proyectos = () => {
+  return(
+    <h1>Saludo Proyectos</h1>
+  );
+}
+
+export default Proyectos;
