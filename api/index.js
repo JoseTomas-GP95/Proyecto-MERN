01e8bd5393e03dfb4e209b264bb5ca5c14a89cc8
tomp <you@example.com>
2020-11-18 12:30:37 -0300

    importando rutas

--- api/index.js
+++ api/index.js
@@ -7,13 +7,14 @@ const app = express();
 // Conectar a la base de datos
 conectarDB();
 
+// Importando rutas
+app.use("/api/usuarios", require("./routes/usuarios"));
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
+app.post("/", (req, res) => {
   res.send("hola mundo")
 })
-
 
 app.listen(PORT, () => {
   console.log(`El servidor corre en el puerto ${PORT}`)
