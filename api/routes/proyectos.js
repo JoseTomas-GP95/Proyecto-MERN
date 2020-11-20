a45455001bc426e0ed3ee967dcbda19fba775a0c
tomp <you@example.com>
2020-11-19 21:07:27 -0300

    Proyecto rutas

--- api/routes/proyectos.js
+++ api/routes/proyectos.js
@@ -4,18 +4,36 @@ const router = express.Router();
 const proyectoController = require("../controllers/proyectosController");
 const auth = require("../middleware/auth");
 const { check } = require('express-validator');
-
+// CREAR PROYECTO
 router.post("/",
-auth,
-[
-  check("nombre", "El nombre del proyecto debe ser ingresado").not().isEmpty()
-],
-proyectoController.crearProyecto
+  auth,
+  [
+    check("nombre", "El nombre del proyecto debe ser ingresado").not().isEmpty()
+  ],
+  proyectoController.crearProyecto
 );
+
+// OBTENER TODOS LOS PROYECTOS
 router.get("/",
-auth,
-proyectoController.obtenerProyectos
-),
+  auth,
+  proyectoController.obtenerProyectos
+);
+
+// ACTULAIZAR O EDITAR PROYECTO (POR ID)
+router.put("/:id", 
+  auth,
+  [
+    check("nombre", "El nombre del proyecto debe ser ingresado").not().isEmpty()
+  ],
+  proyectoController.actualizarProyecto
+);
+
+// ELIMINAR PROYECTO
+router.delete("/:id", 
+  auth,
+  proyectoController.eliminarProyecto
+);
+
 
 
 module.exports = router;
